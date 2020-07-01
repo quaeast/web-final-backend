052b0846d8d4bba34e0e67d11cd4abf8d182baf4
fangzidong <424044876@qq.com>
2020-07-01 22:59:15 +0800

    add api test

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.urls import include, path
 from rest_framework import routers
-from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token
 
 from tutorial.quickstart import views
 
@@ -9,8 +9,8 @@ router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('', include(router.urls)),
     url(r'^api-token-auth/', obtain_jwt_token),
+    url(r'^api-token-verify', verify_jwt_token),
 ]
